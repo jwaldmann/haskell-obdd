9ece69ecb848ad83c5e33da4e944b58e16a4fb4c
Johannes Waldmann <waldmann@imn.htwk-leipzig.de>
2012-08-20 19:54:58 +0200

    file

---
+++ src/OBDD/IntIntMap.hs
@@ -0,0 +1,28 @@
+module OBDD.IntIntMap 
+       
+( IntIntMap ()       
+, empty, lookup, insert
+)  
+       
+where
+
+import Prelude hiding ( lookup )  
+  
+import Data.IntMap (IntMap)
+import qualified Data.IntMap.Strict as M
+
+newtype IntIntMap v = IntIntMap (IntMap (IntMap v))
+
+empty = IntIntMap M.empty
+
+lookup (i,j) (IntIntMap mm) = do
+    m <- M.lookup i mm
+    M.lookup j m
+
+insert (i,j) v (IntIntMap mm) = 
+    case M.lookup i mm of
+        Nothing -> IntIntMap 
+                   $ M.insert i (M.singleton j v) mm
+        Just m -> IntIntMap           
+                   $ M.insert i (M.insert j v m) mm
+                   
