e24b4cb7683eea9df561b2772b2e5704b3d3fb77
Johannes Waldmann <waldmann@imn.htwk-leipzig.de>
2012-08-20 18:01:17 +0200

    version (no counter, IntIntMap)

--- src/OBDD/Data.hs
+++ src/OBDD/Data.hs
@@ -23,6 +23,12 @@ module OBDD.Data
 
 where
 
+import Data.IntMap.Strict ( IntMap )
+import qualified Data.IntMap.Strict as IM
+
+import OBDD.IntIntMap (IntIntMap)
+import qualified OBDD.IntIntMap as IIM
+
 import Data.Map.Strict ( Map )
 import qualified Data.Map.Strict as M
 
@@ -35,18 +41,20 @@ import qualified System.Random
 import Prelude hiding ( null )
 import qualified Prelude
 
-newtype Index = Index { unIndex :: Int }
-   deriving ( Eq, Ord, Num, Enum, Show )
+-- newtype Index = Index { unIndex :: Int }
+--   deriving ( Eq, Ord, Num, Enum, Show )
+
+type Index = Int ; unIndex = id
 
 -- | assumes total ordering on variables
 data OBDD v = OBDD
-	    { core :: !(Map Index ( Node v Index ))
+	    { core :: !(IntMap ( Node v Index ))
 	    , icore :: !(Map ( Node v Index ) Index)
-	    , counter :: Map Index Integer -- ^ number of assignments
 	    , next :: !Index
 	    , top :: !Index
-	    , cache :: ! (Map ( Index, Index ) Index) -- ^ inputs and output for binary op
-		    -- (unary will be simulated by binary)
+	    , cache :: ! (IntIntMap Index) 
+               -- ^ inputs and output for binary op
+               -- (unary will be simulated by binary)
 	    }
 
 size = unIndex . next
@@ -79,12 +87,11 @@ number_of_models vs o =
 
 empty :: OBDD v
 empty = OBDD 
-      { core = M.empty
+      { core = IM.empty
       , icore = M.empty
-      , counter = M.empty
       , next = 0
       , top = -42 -- error "OBDD.Data.empty"
-      , cache = M.empty
+      , cache = IIM.empty
       }
 
 data Node v i = Leaf !Bool
@@ -94,25 +101,22 @@ data Node v i = Leaf !Bool
 {-! for Node derive: ToDoc !-}
 
 access :: OBDD v -> Node v ( OBDD v )
-access s = case M.lookup ( top s ) ( core s ) of
+access s = case IM.lookup ( top s ) ( core s ) of
     Nothing -> error "OBDD.Data.access"
     Just n  -> case n of
         Leaf p -> Leaf p
 	Branch v l r -> 
           Branch v ( s { top = l } ) ( s { top = r } )
 
-count :: OBDD v -> Index -> Integer
-count s i = case M.lookup i ( counter s ) of
-    Nothing -> error "OBDD.Data.count"
-    Just n  -> n
-
 -- | does the OBDD have any models?
 satisfiable :: OBDD v -> Bool
-satisfiable s = 0 < count s ( top s )
+satisfiable = not . null
 
 -- | does the OBDD not have any models?
 null :: OBDD v -> Bool
-null s = 0 == count s ( top s )
+null s = case access s of
+      Leaf False -> True 
+      _ -> False
 
 
 -- | randomly select one model, if possible
@@ -171,12 +175,13 @@ cached :: Ord v
 	-> State ( OBDD v ) Index
 cached (l,r) action = do
     s <- get
-    case M.lookup (l,r) $ cache s of
+    case IIM.lookup (l,r) $ cache s of
         Just i -> return i
 	Nothing -> do
 	    i <- action
 	    s <- get
-	    put $ s { cache = M.insert ( l,r) i $ cache s }
+	    put $ s { cache = IIM.insert ( l,r) i 
+                              $ cache s }
 	    return i
 
 register :: Ord v
@@ -191,12 +196,8 @@ register n = do
             _ -> do
 	        i <- fresh
 	        s <- get
-	        let c = case n of
-		        Leaf p -> if p then 1 else 0
-		        Branch v l r -> count s l + count s r 
 	        put $ s 
-		    { core = M.insert i n $ core s
+		    { core = IM.insert i n $ core s
 		    , icore = M.insert n i $ icore s
-		    , counter = M.insert i c $ counter s
 		    }
 	        return i  
