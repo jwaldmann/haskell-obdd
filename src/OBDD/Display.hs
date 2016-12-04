d3264090b10c7f76d842acd16f82a4df4c133fc2
Johannes Waldmann <@>
2016-12-04 16:52:43 +0100

    add display' function that ignores FALSE

--- src/OBDD/Display.hs
+++ src/OBDD/Display.hs
@@ -16,30 +16,45 @@ import System.Process.Text
 -- Window can be closed gracefully
 -- by typing  'q'  when it has focus.
 display :: (Ord v, Show v) => OBDD v -> IO ()
-display d =
-  void $ readProcessWithExitCode "dot" [ "-Tx11" ] $ toDot d
+display d = void
+  $ readProcessWithExitCode "dot" [ "-Tx11" ]
+  $ toDot False d
+
+-- | same as @display@, but does not show the @False@ node
+-- and the edges pointing to @False@.
+display' :: (Ord v, Show v) => OBDD v -> IO ()
+display' d = void
+  $ readProcessWithExitCode "dot" [ "-Tx11" ]
+  $ toDot True d
 
 -- | a textual representation of the BDD that is suitable
 -- as input to the "dot" program from the graphviz suite.
-toDot :: (Ord v, Show v) => OBDD v -> Text
-toDot = snd . ( \ m -> evalRWS (do tell "digraph BDD {"; m; tell "}" ) () 0 ) . foldM
-  ( \ b -> do
+toDot :: (Ord v, Show v)
+  => Bool -- ^ suppress pointers to False
+  -> OBDD v -> Text
+toDot suppress_false =
+    snd
+  . ( \ m -> evalRWS (do tell "digraph BDD {"; m; tell "}" ) () 0 )
+  . foldM
+  ( \ b -> if not b && suppress_false then return Nothing else do
      this <- fresh
      tell $ T.unlines
        [ "node[shape=rectangle];"
        , text this <> mkLabel (text b)
        , "node[shape=oval];"
        ]
-     return this
+     return $ Just this
   )
-  ( \ v l r -> do
+  ( \ v ml mr -> do
      this <- fresh
-     tell $ T.unlines
-       [ text this <> mkLabel (text v)
-       , text this <> "->" <> text l <> mkLabel "0"
-       , text this <> "->" <> text r <> mkLabel "1"
-       ]
-     return this
+     tell $ text this <> mkLabel (text v)
+     case ml of
+       Just l -> tell $ text this <> "->" <> text l <> mkLabel "0"
+       Nothing -> return ()
+     case mr of
+       Just r -> tell $ text this <> "->" <> text r <> mkLabel "1"
+       Nothing -> return ()
+     return $ Just this
   )
 
 fresh :: Monoid b => RWS a b Int Int
